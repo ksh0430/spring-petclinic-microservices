bd4f520f28980f52cdf01117f555855706abd629
Jin Gi Kong <jgkong@kr.ibm.com>
2017-05-13 14:18:13 +0000

    Remove monitoring, fixes sdevopsadm/roadmap#20
    * Resolve roadmap#3 too

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -18,13 +18,11 @@ package org.springframework.samples.petclinic.customers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.samples.petclinic.monitoring.MonitoringConfig;
 
 /**
  * @author Maciej Szarlinski
  */
 @SpringBootApplication
-@Import(MonitoringConfig.class)
 public class CustomersServiceApplication {
 
 	public static void main(String[] args) {
